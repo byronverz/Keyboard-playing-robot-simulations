fcb0fb99bfa57abe0506d35ad31bb4f16350944e
Byron Verzmoter <byronverzmoter@gmail.com>
2020-10-28 10:44:41 +0200

    AMDF accuracy test 1

--- Analysis/amdf_accuracy_plot.py
+++ Analysis/amdf_accuracy_plot.py
@@ -8,7 +8,8 @@ Created on Tue Oct 20 13:40:26 2020
 import numpy as np
 import matplotlib.pyplot as plt
 
-freqs = [630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 639.1304347826087, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 639.1304347826087, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 639.1304347826087, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0, 630.0]
+with open("out_freq_list.csv", 'r') as freq_file:
+    freqs = np.genfromtxt(freq_file)
 
 with open("key_freq_list.txt", 'r') as kfile:
     key_freq_list = np.genfromtxt(kfile)
@@ -24,12 +25,15 @@ for i in range(len(key_freq_list)-1):
 for i,f_center in enumerate(key_freq_list[1:-1]):
     print(i)
     temp_freqs = freqs[i*48:(i+1)*48]
-    plt.hist(freqs, bins = 9)
+    plt.hist(temp_freqs, bins = 9)
     plt.title("AMDF accuracy test ({})".format(key_name_list[i]))
     plt.vlines(x=[half_points[i], half_points[i+1]], ymin = 0, ymax = 22)
-    plt.text(half_points[i], 25, "Threshold at {} Hz".format(half_points[i]))
-    plt.text(half_points[i+1], 30, "Threshold at {} Hz".format(half_points[i+1]))
-    plt.text(f_center,23, "Target key frequency at {} Hz".format(f_center))
+    plt.text(half_points[i], 25, "Threshold at \n{} Hz".format(int(half_points[i])),
+             horizontalalignment="center",verticalalignment="center", color='k')
+    plt.text(half_points[i+1], 25, "Threshold at \n{} Hz".format(int(half_points[i+1])),
+             horizontalalignment="center",verticalalignment="center",color='k')
+    plt.text(f_center,25, "Target key frequency at \n{} Hz".format(int(f_center)),
+             horizontalalignment="center",verticalalignment="center", color = 'r')
     plt.vlines(x=f_center, ymin = 0 ,ymax = 22, colors='red')
     plt.savefig("AMDF_Accuracy_results/key_{}.png".format(key_name_list[i]))
     plt.close()
