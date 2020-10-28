58056b47d1c7df297e5b454011335e6e46cb1e62
Byron Verzmoter <byronverzmoter@gmail.com>
2020-10-28 15:48:23 +0200

    Trying to improve accuracy with pitch instead of freq

--- Analysis/amdf_accuracy_plot.py
+++ Analysis/amdf_accuracy_plot.py
@@ -22,20 +22,25 @@ for i in range(len(key_freq_list)-1):
     half_points.append((key_freq_list[i]+key_freq_list[i+1])/2)
 
 
-for i,f_center in enumerate(key_freq_list[1:-1]):
-    print(i)
-    temp_freqs = freqs[i*48:(i+1)*48]
-    plt.hist(temp_freqs, bins = 9)
-    plt.title("AMDF accuracy test ({})".format(key_name_list[i]))
-    plt.vlines(x=[half_points[i], half_points[i+1]], ymin = 0, ymax = 22)
-    plt.text(half_points[i], 25, "Threshold at \n{} Hz".format(int(half_points[i])),
-             horizontalalignment="center",verticalalignment="center", color='k')
-    plt.text(half_points[i+1], 25, "Threshold at \n{} Hz".format(int(half_points[i+1])),
-             horizontalalignment="center",verticalalignment="center",color='k')
-    plt.text(f_center,25, "Target key frequency at \n{} Hz".format(int(f_center)),
-             horizontalalignment="center",verticalalignment="center", color = 'r')
-    plt.vlines(x=f_center, ymin = 0 ,ymax = 22, colors='red')
-    plt.savefig("AMDF_Accuracy_results/key_{}.png".format(key_name_list[i]))
-    plt.close()
+# for i,f_center in enumerate(key_freq_list[1:-1]):
+#     print(i)
+#     temp_freqs = freqs[i*48:(i+1)*48]
+#     plt.hist(temp_freqs, bins = 9)
+#     plt.title("AMDF accuracy test ({})".format(key_name_list[i]))
+#     plt.xlabel("Frequency (Hz)")
+#     plt.ylabel(("Number of frequency classifications per bin"))
+#     plt.vlines(x=[half_points[i], half_points[i+1]], ymin = 0, ymax = 22)
+#     plt.text(half_points[i], 25, "Threshold at \n{} Hz".format(int(half_points[i])),
+#              horizontalalignment="center",verticalalignment="center", color='k')
+#     plt.text(half_points[i+1], 25, "Threshold at \n{} Hz".format(int(half_points[i+1])),
+#              horizontalalignment="center",verticalalignment="center",color='k')
+#     plt.text(f_center,25, "Target key frequency at \n{} Hz".format(int(f_center)),
+#              horizontalalignment="center",verticalalignment="center", color = 'r')
+#     plt.vlines(x=f_center, ymin = 0 ,ymax = 22, colors='red')
+#     plt.savefig("AMDF_Accuracy_results/key_{}.png".format(key_name_list[i]))
+#     plt.close()
     
+plt.plot(freqs)
+for f in key_freq_list[1:-1]:
+    plt.axhline(f, color='r')
 
